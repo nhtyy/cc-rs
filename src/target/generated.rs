424acb084e212c330de96e06cab69be0d9df55f4
nhtyy <n@push0.dev>
2025-02-27 10:59:12 -0800

    patch: add succinct target

--- src/target/generated.rs
+++ src/target/generated.rs
@@ -2045,6 +2045,18 @@ pub(crate) const LIST: &[(&str, TargetInfo<'static>)] = &[
         },
     ),
     (
+        "riscv32im-succinct-zkvm-elf",
+        TargetInfo {
+            full_arch: "riscv32im",
+            arch: "riscv32",
+            vendor: "succinct",
+            os: "zkvm",
+            env: "",
+            abi: "",
+            unversioned_llvm_target: "riscv32",
+        },
+    ),
+    (
         "riscv32im-unknown-none-elf",
         TargetInfo {
             full_arch: "riscv32im",
